30753b25a2427910c84ec67c75a5f96a24513e12
anderleand <andersonleandro.desouza@gmail.com>
2021-04-22 15:18:54 -0300

    Finalização do treinamento

--- models/Atendimento.js
+++ models/Atendimento.js
@@ -1,4 +1,5 @@
-const moment = require('moment')
+const moment = require('moment');
+const atendimentos = require('../controllers/atendimentos.js');
 const conexao = require('../infraestrutura/conexao.js')
 
 class Atendimento {
@@ -37,11 +38,63 @@ class Atendimento {
                 if (erro) {
                     res.status(400).json(erro);
                 } else {
-                    res.status(201).json(resultados);
+                    res.status(201).json(atendimento);
                 }
             });
         }
 
+    }
+
+    lista(res) {
+        const sql = 'SELECT * FROM Atendimentos';
+
+        conexao.query(sql, (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(resultados);
+            }
+        })
+    }
+
+    buscaPorId(id, res) {
+        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
+
+        conexao.query(sql, (erro, resultados) => {
+            const atendimento = resultados[0];
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(atendimento);
+            }
+        });
+    }
+
+    altera(id, valores, res) {
+        const sql = 'UPDATE Atendimentos SET ? WHERE id=?';
+
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if (valores.data) {
+                valores.data = moment(valores.data, 'DD-MM-YYYY').format('YYYY-MM-DD HH:mm:ss');
+            }
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json({...valores, id});
+            }
+        });
+    }
+
+    delete(id, res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id=?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if (erro){
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json({id});
+            }
+        });
     }
 }
 
